ab6a50e84855cc266683d4ca05a99a0445a9c2c6
NickChubb <nick@nickchubb.ca>
2024-02-16 21:18:48 -0800

    Add global llm settings

--- app/chatbot.py
+++ app/chatbot.py
@@ -5,6 +5,7 @@ from llama_index import (
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
+    Settings
 )
 from llama_index.llms.openai import OpenAI
 
@@ -17,20 +18,20 @@ if OPENAI_API_KEY is None:
             OPENAI_API_KEY = secret_file.read().strip()
             os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
-llm = OpenAI(temperature=5)
+Settings.llm = OpenAI(temperature=5)
 
 # check if storage already exists
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     documents = SimpleDirectoryReader("data").load_data()
-    index = VectorStoreIndex.from_documents(documents, llm=llm)
+    index = VectorStoreIndex.from_documents(documents)
     # store it for later
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
     # load the existing index
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context, llm=llm)
+    index = load_index_from_storage(storage_context)
 
 query_wrapper = "You are an AI chatbot representing Nick Chubb. \
     You are to answer the following question in first person, 3 - 4 lines only: "
