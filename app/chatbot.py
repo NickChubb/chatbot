ba7a273e5f8850cac0441314d613eb5d87e87285
NickChubb <nick@nickchubb.ca>
2024-02-16 21:28:58 -0800

    Add service context

--- app/chatbot.py
+++ app/chatbot.py
@@ -5,7 +5,7 @@ from llama_index import (
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
-    Settings
+    ServiceContext
 )
 from llama_index.llms.openai import OpenAI
 
@@ -18,20 +18,21 @@ if OPENAI_API_KEY is None:
             OPENAI_API_KEY = secret_file.read().strip()
             os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
-Settings.llm = OpenAI(temperature=5)
+llm = OpenAI(temperature=5)
+service_context = ServiceContext.from_defaults(llm=llm)
 
 # check if storage already exists
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     documents = SimpleDirectoryReader("data").load_data()
-    index = VectorStoreIndex.from_documents(documents)
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
     # store it for later
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
     # load the existing index
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
+    index = load_index_from_storage(storage_context, service_context=service_context)
 
 query_wrapper = "You are to answer the following question in \
     first person as Nick Chubb, a full stack developer, 3 - 4 lines only: "
